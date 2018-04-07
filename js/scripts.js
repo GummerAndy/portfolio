28ede5c8ad558d63f7ffd230da7c1e7951ff4b33
Andy Gummer <gummer.andy@gmail.com>
2018-04-07 10:52:08 +0200

    changed scroll spreed

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,8 @@
 
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        // }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+      }, 200);
 
         // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
